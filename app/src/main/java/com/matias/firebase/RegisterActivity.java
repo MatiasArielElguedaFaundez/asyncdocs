b33c32c74282b322371357a62e7b3893ad792f0b
Pau Vilar I Estrada <148791785+PauVilarEstrada@users.noreply.github.com>
2024-02-05 00:13:58 +0100

    update register now

--- app/src/main/java/com/matias/firebase/RegisterActivity.java
+++ app/src/main/java/com/matias/firebase/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.matias.firebase;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -13,9 +14,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RegisterActivity extends AppCompatActivity {
     private EditText usernameEditText;
@@ -23,6 +28,7 @@ public class RegisterActivity extends AppCompatActivity {
     private Button registerButton;
 
     private FirebaseAuth firebaseAuth;
+    private DatabaseReference usersRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +36,11 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         firebaseAuth = FirebaseAuth.getInstance();
+        usersRef = FirebaseDatabase.getInstance().getReference("users");
 
         usernameEditText = findViewById(R.id.editTextUsername);
         passwordEditText = findViewById(R.id.editTextPassword);
         registerButton = findViewById(R.id.buttonRegister);
-        FirebaseApp.initializeApp(this);
-
 
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,30 +54,36 @@ public class RegisterActivity extends AppCompatActivity {
         String email = usernameEditText.getText().toString().trim();
         String password = passwordEditText.getText().toString().trim();
 
-        if (TextUtils.isEmpty(email)) {
-            Toast.makeText(this, "Por favor, ingrese un correo electrónico", Toast.LENGTH_SHORT).show();
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Por favor, complete todos los campos", Toast.LENGTH_SHORT).show();
             return;
         }
-
-        if (TextUtils.isEmpty(password)) {
-            Toast.makeText(this, "Por favor, ingrese una contraseña", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        if (password.length() < 6) {
-            Toast.makeText(this, "La contraseña debe tener al menos 6 caracteres", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
         firebaseAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            Toast.makeText(RegisterActivity.this, "Registro exitoso", Toast.LENGTH_SHORT).show();
+                            String userId = firebaseAuth.getCurrentUser().getUid();
+                            usersRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    if (!snapshot.child(userId).exists()) {
+                                        usersRef.child(userId).setValue(true);
+                                        Toast.makeText(RegisterActivity.this, "Registro exitoso", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(RegisterActivity.this, DocumentListActivity.class));
+                                        finish();
+                                    } else {
+                                        Toast.makeText(RegisterActivity.this, "El usuario ya está registrado", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+                                    Toast.makeText(RegisterActivity.this, "Error en la base de datos", Toast.LENGTH_SHORT).show();
+                                }
+                            });
                         } else {
                             String errorMessage = task.getException().getMessage();
-                            Toast.makeText(RegisterActivity.this, "Error al registrar. " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            Toast.makeText(RegisterActivity.this, "Error al registrar. " + errorMessage, Toast.LENGTH_SHORT).show();
                             Log.e("Registro Fallido", errorMessage);
                         }
                     }
