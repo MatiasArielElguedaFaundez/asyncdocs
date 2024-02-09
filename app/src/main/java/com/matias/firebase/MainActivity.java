410a35b616875d585220a71c2b27f22adaa23472
MatiasArielElguedaFaundez <117478780+MatiasArielElguedaFaundez@users.noreply.github.com>
2024-02-09 01:48:45 +0100

    todofullstack420palosnegrosklk

--- app/src/main/java/com/matias/firebase/MainActivity.java
+++ app/src/main/java/com/matias/firebase/MainActivity.java
@@ -1,23 +1,17 @@
 package com.matias.firebase;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.fragment_container, new FragmentEditText());
-        transaction.commit();
     }
     @Override
     protected void onStart() {
